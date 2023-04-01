462b206ad540920829c94b8d9b740aafe0a40c8c
hiepietel <jachumail@yahoo.pl>
2023-04-01 14:22:17 +0200

    Controllers update

--- api-dotnet/Controllers/LeadController.cs
+++ api-dotnet/Controllers/LeadController.cs
@@ -3,37 +3,57 @@ using Microsoft.AspNetCore.Mvc;
 namespace api_dotnet.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+//[Route("[controller]")]
 public class LeadController : ControllerBase
 {
+    private readonly ILeadService _leadService;
+    public LeadController(ILeadService leadService)
+    {
+        _leadService = leadService;
+    }
+
     [HttpPost]
     [Route("/api/leads")]
-    public async Task<IActionResult> CreateLead(){
-        
+    public async Task<IActionResult> CreateLead([FromBody] Lead lead)
+    {
+        await _leadService.CreateLead(lead);
         return Ok();
     }
 
     [HttpGet]
     [Route("api/leads")]
-    public async Task<IActionResult> GetLeads(){
-        return Ok();
+    public async Task<IActionResult> GetLeads()
+    {
+        var response = await _leadService.GetLeads();
+        return Ok(response);
     }
 
     [HttpGet]
     [Route("api/leads/{leadId}")]
-    public async Task<IActionResult> GetLead([FromQuery] int leadId){
-        return Ok();
+    public async Task<IActionResult> GetLead([FromQuery] int leadId)
+    {
+        return Ok(await _leadService.GetLead(leadId));
     }
 
     [HttpPut]
     [Route("api/leads/{leadId}")]
-    public async Task<IActionResult> UpdateLead([FromQuery] int leadId){
-        return Ok();
+    public async Task<IActionResult> UpdateLead([FromQuery] int leadId, [FromBody] Lead lead)
+    {
+        await _leadService.UpdateLead(leadId, lead);
+        return Ok(new
+        {
+            message = "Successfully Updated"
+        });
     }
 
     [HttpDelete]
     [Route("api/leads/{leadId}")]
-    public async Task<IActionResult> DeleteLead([FromQuery] int leadId){
-        return Ok();
+    public async Task<IActionResult> DeleteLead([FromQuery] int leadId)
+    {
+        await _leadService.DeleteLead(leadId);
+        return Ok(new
+        {
+            message = "Successfully Deleted"
+        });
     }
 }
