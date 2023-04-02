e73389fc8eeecacfd18892b1ead20572702c12d2
hiepietel <jachumail@yahoo.pl>
2023-04-02 11:34:19 +0200

    UserController

--- api-dotnet/Controllers/UserController.cs
+++ api-dotnet/Controllers/UserController.cs
@@ -6,23 +6,41 @@ namespace api_dotnet.Controllers;
 //[Route("[controller]")]
 public class UserController : ControllerBase
 {
+    private readonly IUserService _userService;
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
 
     [HttpPost]
     [Route("/api/users")]
-    public async Task<IActionResult> CreateUser(){
-        
-        return Ok("test");
+    public async Task<IActionResult> CreateUser([FromBody] UserDto userDto)
+    {
+        var token = await _userService.Register(userDto);
+        return Ok(new
+        {
+            access_token = token,
+            token_type = "bearer"
+        });
     }
 
     [HttpPost]
     [Route("/api/token")]
-    public async Task<IActionResult> GenerateToken(){
-        return Ok();
+    public async Task<IActionResult> GenerateToken([FromBody] UserDto userDto)
+    {
+        var token = await _userService.Login(userDto);
+        return Ok(new
+        {
+            access_token = token,
+            token_type = "bearer"
+        });
     }
 
     [HttpGet]
     [Route("api/users/me")]
-    public async Task<IActionResult> GetUser(){
+    public async Task<IActionResult> GetUser()
+    {
+        await _userService.GetMe();
         return Ok();
     }
 }
