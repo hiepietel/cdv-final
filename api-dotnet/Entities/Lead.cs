98f1f3ab4669933737db9ee2d6be455694540025
hiepietel <jachumail@yahoo.pl>
2023-04-02 11:31:43 +0200

    Lead like in python api

--- api-dotnet/Entities/Lead.cs
+++ api-dotnet/Entities/Lead.cs
@@ -1,11 +1,11 @@
 public class Lead{
     public int Id { get; set; }
     public int OwnerId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Company { get; set; }
-    public string Note { get; set; }
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string email { get; set; }
+    public string company { get; set; }
+    public string note { get; set; }
     public DateTime DateCreated { get; set; }
-    public DateTime DateLastUpdated { get; set; }
+    public DateTime? DateLastUpdated { get; set; }
 }
