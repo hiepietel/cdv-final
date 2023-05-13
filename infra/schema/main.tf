31b6e6d0f1bd92a9b538b5a1f7e1e77ae534c095
hiepietel <jachumail@yahoo.pl>
2023-05-14 00:36:33 +0200

    main.tf

--- infra/schema/main.tf
+++ infra/schema/main.tf
@@ -19,3 +19,33 @@ module "postgres_server" {
   owner       = var.owner
   prefix      = var.prefix
 }
+
+module "web_app_api" {
+  source = "./modules/web_app_api"
+
+  prefix      = var.prefix
+  application = var.application
+  environment = var.environment
+  owner       = var.owner
+
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
+
+  postgres_user     = module.postgres_server.postgres_user
+  postgres_password = module.postgres_server.postgres_password
+  postgres_host     = module.postgres_server.postgres_host
+}
+
+module "web_app_front" {
+  source = "./modules/web_app_front"
+
+  prefix      = var.prefix
+  application = var.application
+  environment = var.environment
+  owner       = var.owner
+
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
+
+  api_url = module.web_app_api.api_url
+}
