0822eb1b426cbb18d5a94942311a28ea553a7fd1
hiepietel <jachumail@yahoo.pl>
2023-05-15 23:46:47 +0200

    change db build

--- api-dotnet/Program.cs
+++ api-dotnet/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppContext>();
+builder.Services.AddDbContext<AppContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddScoped<ILeadService, LeadService>();
 builder.Services.AddScoped<IUserService, UserService>();
