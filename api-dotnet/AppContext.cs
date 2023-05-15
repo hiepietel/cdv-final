534eefee24cf41825ff317d54858362351cdfadf
hiepietel <jachumail@yahoo.pl>
2023-05-16 00:09:39 +0200

    app context 1

--- api-dotnet/AppContext.cs
+++ api-dotnet/AppContext.cs
@@ -2,16 +2,21 @@ using Microsoft.EntityFrameworkCore;
 
 public class AppContext : DbContext
 {
-    private readonly IConfiguration _configuration;
-    public AppContext(IConfiguration configuration)
+    // private readonly IConfiguration _configuration;
+    // public AppContext(IConfiguration configuration)
+    // {
+    //     _configuration = configuration;
+    // }
+    public AppContext(DbContextOptions<AppContext> options)
+        : base(options)
     {
-        _configuration = configuration;
     }
+
     public DbSet<User> Users { get; set; }
     public DbSet<Lead> Leads { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
-    }
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+    // }
 }
