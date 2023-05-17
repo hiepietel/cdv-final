302f632f9d5ace4a2cbe0b0f0fdb5eba4d801901
hiepietel <jachumail@yahoo.pl>
2023-05-18 01:25:39 +0200

    uncomment

--- infra/schema/main.tf
+++ infra/schema/main.tf
@@ -8,17 +8,17 @@ module "resource_group" {
   prefix      = var.prefix
 }
 
-# module "azurerm_mssql_server" {
-#   source = "./modules/mssqlserver_db"
+module "azurerm_mssql_server" {
+  source = "./modules/mssqlserver_db"
 
-#   resource_group_name = module.resource_group.resource_group_name
-#   location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
 
-#   environment = var.environment
-#   application = var.application
-#   owner       = var.owner
-#   prefix      = var.prefix
-# }
+  environment = var.environment
+  application = var.application
+  owner       = var.owner
+  prefix      = var.prefix
+}
 
 
 
@@ -34,25 +34,25 @@ module "resource_group" {
 #   prefix      = var.prefix
 # }
 
-# module "web_app_api" {
-#   source = "./modules/web_app_api"
+module "web_app_api" {
+  source = "./modules/web_app_api"
 
-#   prefix      = var.prefix
-#   application = var.application
-#   environment = var.environment
-#   owner       = var.owner
+  prefix      = var.prefix
+  application = var.application
+  environment = var.environment
+  owner       = var.owner
 
-#   resource_group_name = module.resource_group.resource_group_name
-#   location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
 
-#   # postgres_user     = module.postgres_server.postgres_user
-#   # postgres_password = module.postgres_server.postgres_password
-#   # postgres_host     = module.postgres_server.postgres_host
+  # postgres_user     = module.postgres_server.postgres_user
+  # postgres_password = module.postgres_server.postgres_password
+  # postgres_host     = module.postgres_server.postgres_host
 
-#   sql_user     = module.azurerm_mssql_server.sql_user
-#   sql_password = module.azurerm_mssql_server.sql_password
-#   sql_host     = module.azurerm_mssql_server.sql_host
-# }
+  sql_user     = module.azurerm_mssql_server.sql_user
+  sql_password = module.azurerm_mssql_server.sql_password
+  sql_host     = module.azurerm_mssql_server.sql_host
+}
 
 # module "web_app_front" {
 #   source = "./modules/web_app_front"
