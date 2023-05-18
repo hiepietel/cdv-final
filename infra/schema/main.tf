c2405b98cb8263428f8449ec40600a5768bd30ef
hiepietel <jachumail@yahoo.pl>
2023-05-18 21:42:15 +0200

    main storage

--- infra/schema/main.tf
+++ infra/schema/main.tf
@@ -1,29 +1,15 @@
-module "resource_group" {
-  source = "./modules/resource_group"
+# module "resource_group" {
+#   source = "./modules/resource_group"
 
-  application = var.application
-  environment = var.environment
-  owner       = var.owner
-  location    = var.location
-  prefix      = var.prefix
-}
+#   application = var.application
+#   environment = var.environment
+#   owner       = var.owner
+#   location    = var.location
+#   prefix      = var.prefix
+# }
 
-module "azurerm_mssql_server" {
-  source = "./modules/mssqlserver_db"
-
-  resource_group_name = module.resource_group.resource_group_name
-  location            = var.location
-
-  environment = var.environment
-  application = var.application
-  owner       = var.owner
-  prefix      = var.prefix
-}
-
-
-
-# module "postgres_server" {
-#   source = "./modules/postgres_db"
+# module "azurerm_mssql_server" {
+#   source = "./modules/mssqlserver_db"
 
 #   resource_group_name = module.resource_group.resource_group_name
 #   location            = var.location
@@ -34,28 +20,8 @@ module "azurerm_mssql_server" {
 #   prefix      = var.prefix
 # }
 
-module "web_app_api" {
-  source = "./modules/web_app_api"
-
-  prefix      = var.prefix
-  application = var.application
-  environment = var.environment
-  owner       = var.owner
-
-  resource_group_name = module.resource_group.resource_group_name
-  location            = var.location
-
-  # postgres_user     = module.postgres_server.postgres_user
-  # postgres_password = module.postgres_server.postgres_password
-  # postgres_host     = module.postgres_server.postgres_host
-
-  sql_user     = module.azurerm_mssql_server.sql_user
-  sql_password = module.azurerm_mssql_server.sql_password
-  sql_host     = module.azurerm_mssql_server.sql_host
-}
-
-# module "web_app_front" {
-#   source = "./modules/web_app_front"
+# module "web_app_api" {
+#   source = "./modules/web_app_api"
 
 #   prefix      = var.prefix
 #   application = var.application
@@ -65,15 +31,18 @@ module "web_app_api" {
 #   resource_group_name = module.resource_group.resource_group_name
 #   location            = var.location
 
-#   api_url = module.web_app_api.api_url
+#   sql_user     = module.azurerm_mssql_server.sql_user
+#   sql_password = module.azurerm_mssql_server.sql_password
+#   sql_host     = module.azurerm_mssql_server.sql_host
 # }
+
 
 module "storage_front" {
   source = "./modules/storage_account"
 
-  application = var.application
-  environment = var.environment
-  owner       = var.owner
+  application         = var.application
+  environment         = var.environment
+  owner               = var.owner
   location            = var.location
   resource_group_name = module.resource_group.resource_group_name
 }
