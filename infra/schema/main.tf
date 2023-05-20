9495a2954ed6585e88a2437ea8b7fc9c51f1b445
hiepietel <jachumail@yahoo.pl>
2023-05-20 08:57:18 +0200

    uncomment

--- infra/schema/main.tf
+++ infra/schema/main.tf
@@ -1,40 +1,40 @@
-# module "resource_group" {
-#   source = "./modules/resource_group"
+module "resource_group" {
+  source = "./modules/resource_group"
 
-#   application = var.application
-#   environment = var.environment
-#   owner       = var.owner
-#   location    = var.location
-#   prefix      = var.prefix
-# }
+  application = var.application
+  environment = var.environment
+  owner       = var.owner
+  location    = var.location
+  prefix      = var.prefix
+}
 
-# module "azurerm_mssql_server" {
-#   source = "./modules/mssqlserver_db"
+module "azurerm_mssql_server" {
+  source = "./modules/mssqlserver_db"
 
-#   resource_group_name = module.resource_group.resource_group_name
-#   location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
 
-#   environment = var.environment
-#   application = var.application
-#   owner       = var.owner
-#   prefix      = var.prefix
-# }
+  environment = var.environment
+  application = var.application
+  owner       = var.owner
+  prefix      = var.prefix
+}
 
-# module "web_app_api" {
-#   source = "./modules/web_app_api"
+module "web_app_api" {
+  source = "./modules/web_app_api"
 
-#   prefix      = var.prefix
-#   application = var.application
-#   environment = var.environment
-#   owner       = var.owner
+  prefix      = var.prefix
+  application = var.application
+  environment = var.environment
+  owner       = var.owner
 
-#   resource_group_name = module.resource_group.resource_group_name
-#   location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
 
-#   sql_user     = module.azurerm_mssql_server.sql_user
-#   sql_password = module.azurerm_mssql_server.sql_password
-#   sql_host     = module.azurerm_mssql_server.sql_host
-# }
+  sql_user     = module.azurerm_mssql_server.sql_user
+  sql_password = module.azurerm_mssql_server.sql_password
+  sql_host     = module.azurerm_mssql_server.sql_host
+}
 
 
 module "storage_front" {
